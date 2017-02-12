a526da4f9948ab5cfc951d90ad7ef5740d471d44
Chris Hamons <chris.hamons@gmail.com>
2017-02-12 00:04:06 -0600

    Start sample

---
+++ libtcod-net-sample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using libtcod;
+
+namespace libtcod.sample
+{
+	class Sample
+	{
+		readonly Size SampleScreenSize = new Size (46, 20);
+		readonly Point SampleScreenPosition = new Point (20, 10);
+
+		ConsoleWindow sampleConsole;
+
+		public void Run ()
+		{
+			CustomFontRequest font = new CustomFontRequest ("fonts/consolas10x10_gs_tc.png", CustomFontRequestFontTypes.Grayscale | CustomFontRequestFontTypes.LayoutTCOD, 0, 0);
+			RootConsoleWindow.Setup (80, 50, "C# sample", false, ConsoleRender.SDL, font);
+
+			sampleConsole = RootConsoleWindow.CreateOffscreenConsole (SampleScreenSize);
+
+			do
+			{
+				// To spin GUI
+				Keyboard.CheckForKeypress (KeyPressType.Pressed);
+			}
+			while (!RootConsoleWindow.Instance.IsWindowClosed);
+		}
+
+		static void Main (string[] args)
+		{
+			(new Sample ()).Run ();
+		}
+	}
+}
+
